5cba415a8bb0f4ea3545886677e2954563b6027b
zalia1308 <arslanovazalia2016@gmail.com>
2025-01-05 20:59:11 +0300

    lesson 5

--- 05_lesson/Click on the button with the CSS-class.py
+++ 05_lesson/Click on the button with the CSS-class.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 
 driver.get("http://uitestingplayground.com/classattr")
-search_field = "
+search_field = ("//button[contains(concat(' ', normalize-space(@class), ' '), ' btn-primary ')]")
 driver.find_element(By.XPATH, search_field).click()
 sleep(4)
 driver.quit()
