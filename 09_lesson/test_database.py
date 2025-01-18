940d1a17f6ee8a27ef05a675f87943df5cc817a5
zalia1308 <arslanovazalia2016@gmail.com>
2025-01-18 11:33:04 +0300

    lesson 9_изменение 2

--- 09_lesson/test_database.py
+++ 09_lesson/test_database.py
@@ -24,20 +24,44 @@ def test_select_subject():
 
 def test_create_subject():
     with engine.connect() as db:
-        sql = text("insert into subject(\"subject_title\") values (:new_subject_title)")
-        rows = db.execute(sql, {'new_subject_title':'Zalia'})
-        db.commit()
+        # Начинаем транзакцию
+        db.execute(text("BEGIN"))
+        try:
+              # Добавляем запись
+              sql = text("insert into subject(\"subject_title\") values (:new_subject_title)")
+              rows = db.execute(sql, {'new_subject_title':'Zalia'})
+              # Проверяем, что запись добавлена
+              sql_check = text("SELECT * FROM subject WHERE subject_title = :title")
+              row = db.execute(sql_check, {'title': 'Zalia'}).fetchone()
+              assert row, "Subject 'Zalia' was added"
+        finally:
+              # Откатываем транзакцию
+              db.execute(text("ROLLBACK"))
 
 
 def test_update_subject():
     with engine.connect() as db:
-        sql = text("update subject set subject_id = :id where subject_title = :title")
-        rows = db.execute(sql, {'id': 25, 'title' : 'Zalia'})
-        db.commit()
+        # Начинаем транзакцию
+        db.execute(text("BEGIN"))
+        try:
+              # Добавляем запись
+              sql_create = text("insert into subject(\"subject_title\") values (:new_subject_title)")
+              rows_create = db.execute(sql_create, {'new_subject_title':'Zalia'})
+              # Изменяем запись
+              sql_update = text("update subject set subject_id = :id where subject_title = :title")
+              rows_update = db.execute(sql_update, {'id': 25, 'title': 'Zalia'})
+              # Проверяем, что запись изменена
+              sql_check = text("SELECT * FROM subject WHERE subject_id = :id")
+              row = db.execute(sql_check, {'id': '25'}).fetchone()
+              assert row, "Subject '25' was added"
+        finally:
+              # Откатываем транзакцию
+              db.execute(text("ROLLBACK"))
 
 
 def test_delete_subject():
     with engine.connect() as db:
-        sql = text("delete from subject where subject_id = :id")
-        rows = db.execute(sql, {'id': 25})
-        db.commit()
+        sql_create = text("insert into subject(\"subject_title\") values (:new_subject_title)")
+        rows_create = db.execute(sql_create, {'new_subject_title': 'Zalia'})
+        sql_delete = text("delete from subject where subject_title = :new_subject_title")
+        rows_delete = db.execute(sql_delete, {'new_subject_title': 'Zalia'})
